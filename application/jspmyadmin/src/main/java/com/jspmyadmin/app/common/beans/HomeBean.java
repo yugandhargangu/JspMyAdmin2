08c5bb3336709fd0442d6a6fc3776f9f862acc7a
Yugandhar Gangu <yugandhargangu@Yugandhars-Mac-mini.local>
2017-08-07 17:36:40 +0530

    Added version update

--- application/jspmyadmin/src/main/java/com/jspmyadmin/app/common/beans/HomeBean.java
+++ application/jspmyadmin/src/main/java/com/jspmyadmin/app/common/beans/HomeBean.java
@@ -42,7 +42,7 @@ public class HomeBean extends Bean {
 	private String servelt_version = null;
 	private String jsp_version = null;
 
-	private String jma_version = "1.0.3";
+	private String jma_version = "1.0.4";
 
 	private String action = null;
 
